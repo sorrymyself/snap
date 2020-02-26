68ecfaaea1d470c3c5c16e58b94ed23db41bef60
root <root@shun.0yfwcwzzloiehplhxlr4bskuga.hx.internal.cloudapp.net>
2020-02-26 19:16:40 +0000

    snap2htmlDailyUpdate

--- snap.sh
+++ snap.sh
@@ -5,8 +5,8 @@ sed -i "s#\[LINK ROOT\]#https://ak.bk201.cf#g"  /shun/snap2html/DiogenesList-mas
 sed -i "s#\[LINK PROTOCOL\]##g"  /shun/snap2html/DiogenesList-master/index.html
 sed -i "s#\[SOURCE ROOT\]##g"  /shun/snap2html/DiogenesList-master/index.html
 sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\00##g"  /shun/snap2html/DiogenesList-master/index.html
-sed -i "s#1>index#1>呵哒!劳资转身就是一脚!#g" /shun/snap2html/DiogenesList-master/index.html
-sed -i "s#e>index#e>小圈子影视分享#g" /shun/snap2html/DiogenesList-master/index.html
+#sed -i "s#1>index#1>呵哒!劳资转身就是一脚!#g" /shun/snap2html/DiogenesList-master/index.html
+#sed -i "s#e>index#e>小圈子影视分享#g" /shun/snap2html/DiogenesList-master/index.html
 cp /shun/snap2html/DiogenesList-master/index.html /shun/snapGit/
 cd /shun/snapGit
 git add .
