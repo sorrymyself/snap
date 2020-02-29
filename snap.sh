823c62196e9668edc75dc36a91acaadf629f86b6
root <root@shun.0yfwcwzzloiehplhxlr4bskuga.hx.internal.cloudapp.net>
2020-02-29 10:36:44 +0000

    test

--- snap.sh
+++ snap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cd /shun/snap2html/DiogenesList-master/
-python /shun/snap2html/DiogenesList-master/diogeneslist.py /shun/GD index
-sed -i "s#\[LINK ROOT\]#https://ak.bk201.cf#g"  /shun/snap2html/DiogenesList-master/index.html
-sed -i "s#\[LINK PROTOCOL\]##g"  /shun/snap2html/DiogenesList-master/index.html
-sed -i "s#\[SOURCE ROOT\]##g"  /shun/snap2html/DiogenesList-master/index.html
-sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\00##g"  /shun/snap2html/DiogenesList-master/index.html
+tmux a -t snap
+python3 /shun/snap2html/DiogenesList-master/diogeneslist.py /root/rclone/00 index
+#sed -i "s#\[LINK ROOT\]#https://ak.bk201.cf#g"  /shun/snap2html/DiogenesList-master/index.html
+#sed -i "s#\[LINK PROTOCOL\]##g"  /shun/snap2html/DiogenesList-master/index.html
+#sed -i "s#\[SOURCE ROOT\]##g"  /shun/snap2html/DiogenesList-master/index.html
+#sed -i "s#\\\\\\\root\\\\\\\rclone\\\\\\\00##g"  /shun/snap2html/DiogenesList-master/index.html
 #sed -i "s#1>index#1>呵哒!劳资转身就是一脚!#g" /shun/snap2html/DiogenesList-master/index.html
 #sed -i "s#e>index#e>小圈子影视分享#g" /shun/snap2html/DiogenesList-master/index.html
 cp /shun/snap2html/DiogenesList-master/index.html /shun/snapGit/
